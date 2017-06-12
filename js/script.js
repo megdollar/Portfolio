cd1849aedbdd2952b3abb0f173fce736c8c19770
Megan Dollar <megdollar@gmail.com>
2017-06-12 15:39:07 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,53 @@
+$(document).ready(function() {
+    
+var name = "Megan Dollar";
+var subTitle = "Front-end Developer";
 
+var pageTitle ="Projects Porfolio";
+
+var logoImage = "images/udacity.png"
+var mainImage ="images/megan1.png"
+
+var projects = [{
+  screenshot: "images/tiki.png",
+  title: "Tiki Quiz", 
+  link: "https://megdollar.github.io/TikiApp/",
+  repo: "https://github.com/megdollar/TikiApp" 
+},
+{
+  screenshot: "images/map.png",
+  title: "Neigborhood Map", 
+  link: "https://megdollar.github.io/Neighborhood_Map/",
+  repo: "https://github.com/megdollar/Neighborhood_Map"  
+},
+{
+  screenshot: "images/resume1.png",
+  title: "Resume", 
+  link: "https://megdollar.github.io/",
+  repo: "https://github.com/megdollar/megdollar.github.io"  
+}];
+    
+$(".udacity").attr("src", logoImage); 
+$(".name").html(name);
+$(".nick-name").html(subTitle);
+$(".content_title").html(pageTitle);
+$(".photo1").attr("src",mainImage);
+ 
+var description = $(".description");
+var screenshot = $(".screenshot");
+var github = $(".github");
+var repository = $(".code");
+
+projectsDisplay = function(){
+	for (var i = 0; i < projects.length; i++){
+		$(description[i]).html(projects[i].title);
+		$(screenshot[i]).attr("src", projects[i].screenshot);
+		$(github[i]).attr("href", projects[i].link);
+		$(repository[i]).attr("href", projects[i].repo);
+	}
+}
+
+projectsDisplay()
+
+
+});
